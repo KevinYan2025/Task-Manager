6ca617dc5758cd81a54cc316b304f277cacc76b1
Kevin Yan <yan767957598@gmail.com>
2023-06-01 10:39:03 -0400

    before deployment

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose')
 
+<<<<<<< HEAD
 mongoose.connect(process.env.MONGOOSE_API_KEY)
 .then(() => {
     
 })
+=======
+mongoose.connect('mongodb+srv://admin:TqSxMR87@zhixiangyan.xljb7t8.mongodb.net/?retryWrites=true&w=majority')
+>>>>>>> parent of 80b12c8 (welcome email and enviroment variable)
