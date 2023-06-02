511a292e437f60e31e840d470ec4999a19d4abb4
Kevin Yan <yan767957598@gmail.com>
2023-06-01 23:47:13 -0400

    sd

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,13 +1,3 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
-  .then(() => {
-    console.log('Connected to MongoDB')
-    // Further code for your application
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error)
-  });
+mongoose.connect(process.env.MONGODB_URL)
