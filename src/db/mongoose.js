e0026e709bf11c9af9b95984599b25fb57cd387c
Kevin Yan <yan767957598@gmail.com>
2023-06-01 21:59:36 -0400

    deploying

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,13 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI)
-
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('Connected to MongoDB')
+    // Further code for your application
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error)
+  });
